329b23c2f5c0f9eab069f731cd2e17ccdca6586c
zsoit <wsparcie.exe@gmail.com>
2020-12-09 12:20:18 +0100

    PHP 2.1

--- src/php/content.php
+++ src/php/content.php
@@ -3,8 +3,8 @@
 
 <!-- (a[href="http://picsum.photos/66$/541/" data-lightbox="mygallery" data-title="Zdjęcie $"]>img[class="item" src="http://picsum.photos/66$/541/" alt="Zdjęcie $"])*8 -->
 <!-- (a[href="images/2020/$.jpg" data-lightbox="mygallery" data-title="Zdjęcie $"]>img[class="item" src="images/2020/$.jpg" alt="Zdjęcie $"])*8 -->
-<div class="gallery " style="display: none; "> 
-  <h2>Studniówka 2020</h2> 
+<div class="gallery " style="display: none; ">
+  <h2>Studniówka 2020</h2>
   <p>W 2020 r. odbyła się uroczysta studniówka  klas technikalnych oraz licealnych. Studniówka to pierwszy, prawdziwy bal w życiu każdego ucznia klas maturalnych. Była szalona zabawa, dobra muzyka i sto dni do matury, te elementy dominowały na naszej studniówce. Bal rozpoczęto przemową uczniów, gdzie padły słowa podziękowań dla Dyrekcji Szkoły, wychowawców klas, nauczycieli oraz rodziców. Następnie wszyscy uczniowie zatańczyli poloneza. </p>
         <a href="images/2020/1.jpg" data-lightbox="mygallery" data-title="Zdjęcie 1"><img src="images/2020/1.jpg" alt="Zdjęcie 1" class="item"></a>
         <a href="images/2020/2.jpg" data-lightbox="mygallery" data-title="Zdjęcie 2"><img src="images/2020/2.jpg" alt="Zdjęcie 2" class="item"></a>
