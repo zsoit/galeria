a179a8dc2de99ceded57cd29ae97d4c89b53b27a
zsoit <wsparcie.exe@gmail.com>
2020-12-10 12:05:54 +0100

    PHP 2.6

--- layout/nav.php
+++ layout/nav.php
@@ -12,7 +12,7 @@
         <div class="navigation">
             <ul class="navigation__list">
                 <span id="userInfo"></span>
-                <li class="navigation__item"><a href="/">Strona Główna</a></li>
+                <li class="navigation__item"><a href="/"> <i class="fas fa-home"></i> Strona Główna</a></li>
                 <li class="navigation__item"><a href="/">Album 2020</a></li>
                 <li class="navigation__item"><a href="/">Album 2019</a></li>
                 <li class="navigation__item"><a href="/">Album 2018</a></li>
@@ -22,6 +22,9 @@
                 <li class="navigation__item"><a href="/">Album 2014</a></li>
                 <li class="navigation__item"><a href="/">Album 2013</a></li>
                 <li class="navigation__item"><a href="/">Albumy Archiwalne</a></li>
+                <li class="navigation__item"><a href="/login.php">Logowanie</a></li>
+                <li class="navigation__item"><a href="/admin.php">Admin</a></li>
+
             </ul>
         </div>
     </div>
---
+++ php/home-admin.php
@@ -0,0 +1,55 @@
+<div class="AdminContanier">
+        <div id="infoClient">
+            <fieldset>
+            <legend>Aktualna sesja </legend>
+                <p>Użytkownik: Eugeniusz</p>
+                <p>Grupa: Admin</p>
+                <p class="infoOS"></p>
+                <p class="infoBrowser"></p>
+                <p class="infoScreen"></p>
+                <p id="ip" class="ipHost"></p> <br> <br> <hr>
+                <div class="navMenu">
+                    <a href="" ><abbr title="Wyloguj się z sesji"><i class="fas fa-sign-out-alt navMenu__item" ></i></abbr> </a>
+                    <a href="" ><abbr title="Dziennik logowania"><i class="fas fa-book-open navMenu__item" ></i></abbr> </a>
+                    <a href="" ><abbr title="Wiadomości"><i class="far fa-comments  navMenu__item" ></i></abbr> </a>
+                    <a href="" ><abbr title="Powiadomienia"><i class="far fa-bell navMenu__item" ></i></abbr> </a>
+                    <a href="" ><abbr title="Dziennik logowania"><i class="fas fa-user-shield navMenu__item" ></i></abbr> </a>
+                </div>
+            </fieldset>
+        </div>
+        <div id="infoClient">
+            <fieldset>
+                <legend>Serwer WWW</legend>
+                <div class="iconWWW" style="margin-left: 10px; padding:  17px ">
+                    <i class="icon-debian" style="color: red; font-size: 45px; "></i>
+                    <i class="icon-apache" style="color: white; font-size: 45px; "></i>
+                    <i class="icon-php" style="color: lightblue; font-size: 45px; "></i>
+                    <i class="icon-mysql" style="color: yellow; font-size: 45px; "></i>
+                    <i class="icon-shell" style="color: green; font-size: 45px; "></i>
+                </div>
+                <p>System: Debian GNU/Linux 10 (buster) i686</p>
+                <p>Kernel: 4.19.0-11-686-pae</p>
+                <p>Shell: bash 5.0.3</p>
+                <p>CPU: Intel Xeon 3.20GHz (4) @ 3.198GHz</p>
+                <p>GPU: AMD ATI Rage XL PCI </p>
+                <p>RAM: 3869MiB</p>
+                <p>Apache/2.4.38 </p>
+                <p>PHP 7.3.19 & mySQL 14.14 </p>
+            </fieldset>
+        </div>
+
+        <div id="infoClient">
+            <fieldset>
+            <legend>Administracja</legend>
+                <div class="navMenu">
+                    <a href=""><abbr title="Dodaj zdjęcie"><i class="fas fa-plus navMenu__item" id="add"></i> <br></abbr></a>
+                    <a href="""> <abbr title="Usuń zdjęcie"><i class="fas fa-times navMenu__item" ></i></a> </abbr>
+                    <a href=""><abbr title="Dodaj użytkownika"><i class="fas fa-user-plus navMenu__item" "></i> </abbr></a>
+                    <a href=""> <abbr title="Usuń użytkownika"> <i class="fas fa-user-slash navMenu__item" ></i></abbr></a>
+                    <a href=""><abbr title="Edytuj uprawnienia"><i class="fas fa-user-edit navMenu__item" ></i></abbr></a>
+
+                </div>
+            </fieldset>
+        </div>
+
+    </div>
